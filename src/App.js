6e4eafb3125b19da33096931836a47f91d746c90
Jakub Szajner <jakub280@gmail.com>
2019-01-09 19:49:05 +0100

    Added simple map with button.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactMapGL from 'react-map-gl';
 import './App.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const API_TOKEN = 'pk.eyJ1IjoiamFrdWIyODAiLCJhIjoiY2pxbzdxeXlzMHN6ZzN3bm9zMXkzMWJ0MiJ9.h-GMX-Ih2pF-WDDGuHuVmQ';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    state = {
+        viewport: {
+            width: '100%',
+            height: 750,
+            latitude: 52.181103,
+            longitude: 21.022131,
+            zoom: 13
+        }
+    };
+
+    _goToPan = () => {
+        const viewport = {...this.state.viewport, longitude: 21.010749, latitude: 52.230258, zoom: 18};
+        this.setState({viewport});
+    }
+
+    render() {
+
+        const {viewport} = this.state;
+
+        return (
+            <div>
+                <ReactMapGL {...viewport}
+                            mapStyle={'mapbox://styles/mapbox/navigation-preview-night-v4'}
+                            mapboxApiAccessToken={API_TOKEN}
+                            {...this.state.viewport} onViewportChange={(viewport) => this.setState({viewport})}>
+
+                </ReactMapGL>
+
+                <button onClick={this._goToPan}>Pokaż patelnię</button>
+            </div>
+        );
+    }
 }
 
 export default App;
+
+// https://stackoverflow.com/questions/38111157/react-map-gl-map-does-not-appear
+// https://www.mapbox.com/api-documentation/#styles
